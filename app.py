70da7ad992361b20b7847c32809682c190c086e7
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-04-30 02:38:07 -0400

    added data visualization

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import sp
+import px
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tracks.db'
+db = SQLAlchemy(app)
+
+class Track(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    track_name = db.Column(db.String(50), nullable = False)
+    track_id = db.Column(db.String(50), nullable = False)
+    artist_name = db.Column(db.String(50), nullable = False)
+    artist_id = db.Column(db.String(50), nullable = False)
+    album = db.Column(db.String(50), nullable = False)
+    released_date = db.Column(db.String(50), nullable = False)
+    acousticness = db.Column(db.Float, nullable = False)
+    danceability = db.Column(db.Float, nullable = False)
+    energy = db.Column(db.Float, nullable = False)
+    instrumentalness = db.Column(db.Float, nullable = False)
+    liveness = db.Column(db.Float, nullable = False)
+    loudness = db.Column(db.Float, nullable = False)
+    speechiness = db.Column(db.Float, nullable = False)
+    tempo = db.Column(db.Float, nullable = False)
+    
+    date_searched = db.Column(db.DateTime, default = datetime.utcnow)
+
+
+    def __repr__(self):
+        return '<Profile %r>' % self.id
+
+@app.route('/')
+def index():
+    ''' home page '''
+    return render_template('index.html')
+
+# @app.route('/search-result')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- sp.py
+++ sp.py
@@ -8,6 +8,8 @@ export SPOTIPY_CLIENT_SECRET=eecbb95959074abb8d773a216294b6b0
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from pprint import pprint
+import pandas as pd
+import matplotlib.pyplot as plt
 
 client_id = '146abbf4a23e4b4d9ecb410a9923a35f'
 client_secret = 'eecbb95959074abb8d773a216294b6b0'
@@ -22,22 +24,47 @@ def search_song(query):
     return a dict with its song name, song id, artist, artist id, album, and release date
     '''
     r = sp.search(query, type='track', limit=1)
-    song = r['tracks']['items'][0]['name']
-    song_id = r['tracks']['items'][0]['id']
-    artist = r['tracks']['items'][0]['artists'][0]['name']
-    artist_id = r['tracks']['items'][0]['artists'][0]['id']
-    album = r['tracks']['items'][0]['album']['name']
-    release_date = r['tracks']['items'][0]['album']['release_date']
-    song_info = {'song': song, 'song_id': song_id, 'artist': artist, 'artist_id': artist_id, 'album': album, 'release_date':release_date}
+    try:
+        song = r['tracks']['items'][0]['name']
+        song_id = r['tracks']['items'][0]['id']
+        artist = r['tracks']['items'][0]['artists'][0]['name']
+        artist_id = r['tracks']['items'][0]['artists'][0]['id']
+        album = r['tracks']['items'][0]['album']['name']
+        release_date = r['tracks']['items'][0]['album']['release_date']
+        song_info = {'song': song, 'song_id': song_id, 'artist': artist, 'artist_id': artist_id, 'album': album, 'release_date':release_date}
+    except:
+        song_info = {}
     return song_info
 
 def song_features(id):
     '''
     returns a dictionary with a track's features, searcing with the track's id
+    id is a string
     '''
-    r = sp.audio_features(id)
-    features = r[0]
-    return features
+    r = sp.audio_features([id])
+
+    return r[0]
+
+def features_visualization(feature_dict):
+    '''
+    features - a dictionary of track features
+    prints a bar plot of all features
+    '''
+    # exclude some features
+    for feature in ['tempo','duration_ms','analysis_url', 'id', 'track_href', 'type', 'uri']:
+        del feature_dict[feature]
+
+    # convert scalar values into lists
+    for feature, value in feature_dict.items():
+        value_list = [value]
+        feature_dict[feature] = value_list
+
+    # creat pandas DataFrame and plot bar chart
+    df = pd.DataFrame(feature_dict)
+    row = df.iloc[0]
+    row.plot(kind='bar', x='Features', y='Values', title='Track Features')
+    plt.show()
+
 
 def recommendations(song_id='', artist_id=''):
     '''
@@ -56,12 +83,14 @@ def recommendations(song_id='', artist_id=''):
     
 
 def main():
-    song_info = search_song('baby justin bieber')
+    song_info = search_song('cradles sub urban')
     print(song_info)
-    features = song_features(song_info['song_id'])
-    print(features)
-    rec = recommendations(song_info['song_id'], song_info['artist_id'])
-    print(rec)
+    if song_info:
+        features = song_features(song_info['song_id'])
+        pprint(features)
+        rec = recommendations(song_info['song_id'], song_info['artist_id'])
+        print(rec)
+        features_visualization(features)
 
 
 if __name__ == "__main__":
