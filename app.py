1ee2c8c87957d39534c08d729824d388c318615c
queeniekwan <60158228+queeniekwan@users.noreply.github.com>
2020-05-04 03:42:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import pexels as px
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def index():
     ''' home page '''
     return render_template('index.html')
 
-@app.route('/search-song/', methods=['POST'])
+@app.route('/search-song/', methods=['GET','POST'])
 def search():
     '''get result for a search'''
     if request.method == 'POST':
